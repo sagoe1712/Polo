360e727d81c7bed2b7216affb651902720ef2915
sagoe1712 <sagoe.olayinka@gmail.com>
2017-09-07 17:05:31 +0100

    replace

--- js/my-app.js
+++ js/my-app.js
@@ -39,6 +39,33 @@ $$(document).on('pageInit', function (e) {
 		}
 		});
 		
+		$('#btn-scan').click(function(){
+			 var scanner = cordova.plugins.barcodeScanner;
+
+        scanner.scan( function (result) { 
+
+            alert("We got a barcode\n" + 
+            "Result: " + result.text + "\n" + 
+            "Format: " + result.format + "\n" + 
+            "Cancelled: " + result.cancelled);  
+
+           console.log("Scanner result: \n" +
+                "text: " + result.text + "\n" +
+                "format: " + result.format + "\n" +
+                "cancelled: " + result.cancelled + "\n");
+            document.getElementById("info").innerHTML = result.text;
+            console.log(result);
+            /*
+            if (args.format == "QR_CODE") {
+                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+            }
+            */
+
+        }, function (error) { 
+            console.log("Scanning failed: ", error); 
+		});
+		
+		
 		$('a.backbutton').click(function(){
 			parent.history.back();
 			return false;
